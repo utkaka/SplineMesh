c38d3de272ab7cce8b5389cd21ed464a39d0b498
Anton Alexeyev <anton@gameonproduction.com>
2022-03-31 22:15:56 +0700

    Change MeshVertex from class to struct for less allocations

--- Assets/SplineMesh/Scripts/Runtime/MeshProcessing/MeshVertex.cs
+++ Assets/SplineMesh/Scripts/Runtime/MeshProcessing/MeshVertex.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace SplineMesh {
     [Serializable]
-    public class MeshVertex {
+    public struct MeshVertex {
         public Vector3 position;
         public Vector3 normal;
         public Vector2 uv;
